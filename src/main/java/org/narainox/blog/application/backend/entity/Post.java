7d56737dc56bf22bf9c8c6528590cb12bfcf57d8
Ayush Shrivastava <ayusrivastava2k3@gmail.com>
2023-12-07 22:34:24 +0530

    Day 4

---
+++ src/main/java/org/narainox/blog/application/backend/entity/Post.java
@@ -0,0 +1,15 @@
+package org.narainox.blog.application.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "posts")
+public class Post {
+}
