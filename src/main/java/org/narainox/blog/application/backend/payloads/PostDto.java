ee38ae26ac33ff538aecbbb4f9c0eb4a7e836f5e
Ayush Shrivastava <ayusrivastava2k3@gmail.com>
2023-12-15 23:38:26 +0530

    Day 9

--- src/main/java/org/narainox/blog/application/backend/payloads/PostDto.java
+++ src/main/java/org/narainox/blog/application/backend/payloads/PostDto.java
@@ -3,8 +3,7 @@ package org.narainox.blog.application.backend.payloads;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.narainox.blog.application.backend.entity.Category;
-import org.narainox.blog.application.backend.entity.User;
+
 
 import java.util.Date;
 
